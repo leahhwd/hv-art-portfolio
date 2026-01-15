8f6e504789cf10e3e6692cf95f73aa89280daf08
leahmkh <105560625+leahmkh@users.noreply.github.com>
2026-01-15 11:47:20 -1000

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './sections/home/home.component';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './sections/about/about.component';
+import { GalleryComponent } from './sections/gallery/gallery.component';
+import { ContactComponent } from './sections/contact/contact.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HomeComponent,
+    CommonModule,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    FooterComponent,
+    NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  menuOpen: boolean = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+}
