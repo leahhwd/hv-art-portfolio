09284c1895df36d162269c0457d0ef1d92e1ea0b
leahmkh <105560625+leahmkh@users.noreply.github.com>
2026-02-03 15:27:15 -1000

    contact component and gallery repair

--- src/app/layout/navbar/navbar.component.spec.ts
+++ src/app/layout/navbar/navbar.component.spec.ts
@@ -20,4 +20,9 @@ describe('NavbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a title "My App"', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.navbar-title')?.textContent).toContain('My App');
+  });
 });
--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Message Submitted!', form.value);
+      form.reset();
+    }
+  }
 
 }
